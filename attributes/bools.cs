9ba0a437a4c7c2518279b74fd5371cd7e1c92584
hee082024kh <hee082024@gmail.com>
2024-12-09 10:36:52 +0100

    Added donatesToCharity and washesHands switches

--- attributes/bools.cs
+++ attributes/bools.cs
@@ -6,7 +6,27 @@ public int naughtyOrNice;
 switch (toiletPaperOutward)
 {
   case true:
+    naughtyOrNice = +10;
     break;
   case false:
+    naughtyOrNice = -10;
+    break;
+}
+
+switch (donatesToCharity)
+{
+  case true:
+    naughtyOrNice = +20
+    break;
+  case false:
+    break;
+}
+
+switch (washesHands)
+{
+  case true:
+    break;
+  case false:
+    naughtyOrNice = -20
     break;
 }

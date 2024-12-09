2cefc28f9945f3d79a277a8809e08da7fafaf51e
hee082024kh <hee082024@gmail.com>
2024-12-09 11:35:20 +0100

    Added more options to CarModels switch case

---
+++ attributes/CarModels.cs
@@ -0,0 +1,82 @@
+public class CarModels
+{
+  Bools bools;
+  switch (carModels)
+  {
+    case "Volkswagen Jetta":
+      bools.naughtyOrNice =+5
+      break;
+    case "BMW Z4":
+      break;
+    case "Kia Stinger":
+      bools.naughtyOrNice =-5
+      break;
+    case "Toyota Supra":
+      bools.naughtyOrNice =+10
+      break;
+    case "Kia Forte":
+      bools.naughtyOrNice =-5
+      break;
+    case "Jaguar XF":
+      bools.naughtyOrNice =-10
+      break;
+    case "Chevrolet Spark":
+      bools.naughtyOrNice =+5
+      break;
+    case "Toyota Yaris":
+      bools.naughtyOrNice =-5
+      break;
+    case "Kia Optima":
+      bools.naughtyOrNice =-5
+      break;
+    case "Ford Fiesta":
+      bools.naughtyOrNice =+5
+      break;
+    case "Ford Focus":
+      bools.naughtyOrNice =+10
+      break;
+    case "Chrysler 300":
+      break;
+    case "Porsche Panamera":
+      bools.naughtyOrNice =-20
+      break;
+    case "Nissan Altima":
+      bools.naughtyOrNice =+10
+      break;
+    case "Chevrolet Cruze":
+      break;
+    case "":
+      bools.naughtyOrNice =+10
+      break;
+    case "":
+      bools.naughtyOrNice =+10
+      break;
+  }
+}
+Mercedes - Benz E - Class
+Subaru BRZ
+Chevrolet Malibu
+Volkswagen Golf
+Subaru Impreza
+Nissan Maxima
+Hyundai Veloster
+Alfa Romeo Giulia
+Mazda MX-5 Miata
+Toyota Camry
+Nissan 370Z
+Honda Fit
+Hyundai Sonata
+Volvo S60
+Dodge Charger
+Audi A6
+BMW 3 Series
+Mercedes-Benz C-Class
+Audi TT
+Ford Fusion
+Tesla Model 3
+BMW 5 Series
+Dodge Challenger
+Toyota Corolla
+Mercedes-Benz SLK
+Ford Mustang
+Honda Accord

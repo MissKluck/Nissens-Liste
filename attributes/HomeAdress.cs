0cb821e9a2b62bcc65741f2c411539453f119336
Amaliebra <amaliebratland@gmail.com>
2024-12-09 12:51:41 +0100

    setup for homeadress

--- attributes/HomeAdress.cs
+++ attributes/HomeAdress.cs
@@ -2,6 +2,43 @@ namespace SantasListGenerator.Address
 {
     public class HomeAdress
     {
-
-    }
+        switch (homeAdress)
+        {
+            case "Kalfarveien";
+            case "Abels gate";
+            case "Absalon Beyers gate";
+            case "Allégaten";
+            case "Allehelgens gate";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+                naughtyOrNice += 10;
+                break;
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+            case "";
+                naughtyOrNice -= 10;
+                break;
+        }
+}
 }

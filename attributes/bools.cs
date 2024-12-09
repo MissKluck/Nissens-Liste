1f5fb2a7436826a0aa5272faf091c668fc534a31
hee082024kh <hee082024@gmail.com>
2024-12-09 10:10:24 +0100

    Added naughtyOrNice variable and toiletPaperOutward switch

--- attributes/bools.cs
+++ attributes/bools.cs
@@ -1,3 +1,12 @@
 public bool toiletPaperOutward;
 public bool donatesToCharity;
 public bool washesHands;
+public int naughtyOrNice;
+
+switch (toiletPaperOutward)
+{
+  case true:
+    break;
+  case false:
+    break;
+}
